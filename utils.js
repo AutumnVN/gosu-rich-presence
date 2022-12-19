960d7fbc43a72a6a9dea17b3375ee64487db7427
Đỗ Văn Hoài Tuân <htuan03@gmail.com>
2022-12-19 13:17:54 +0700

    Update utils.js

--- utils.js
+++ utils.js
@@ -2,10 +2,10 @@ let getLetterGrade = (data) => {
   let hasHidden = data.menu.mods.str.match(/HD/g);
   let letter = data.gameplay.hits.grade.current;
   if (letter === 'SS' && hasHidden)
-    return 'xh'
+    return 'https://autumnvn.me/xh.png'
   if (letter === 'S' && hasHidden)
-    return 'sh'
-  return letter.toLowerCase()
+    return 'https://autumnvn.me/sh.png'
+  return `https://autumnvn.me/${letter.toLowerCase()}.png`
 }
 let resolveObjectPath = (obj, path) => {
   let pathArray = path.split('.');
